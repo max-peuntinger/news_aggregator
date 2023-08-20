5b076bbfb833c5a06f57360ac516e31e0e28d965
mxpnt <maxpeuntinger@gmail.com>
2023-08-20 14:56:37 +0200

    added skeleton ui

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -4,9 +4,8 @@
   </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-		<Articles />
+	<h1>News Aggregator</h1>
+	<Articles />
 </main>
 
 <style>
--- svelte-app/src/Articles.svelte
+++ svelte-app/src/Articles.svelte
@@ -5,16 +5,18 @@
 
   onMount(async () => {
     const response = await fetch('http://localhost:8000/articles');
-    articles = await response.json(); // Assign the JSON response to the articles variable
+    articles = await response.json();
   });
 </script>
 
-<h1>Latest News Articles</h1>
-<ul>
+<div class="container">
+  <h1>Latest News Articles</h1>
   {#each articles as article}
-    <li>
-      <a href={article.link} target="_blank">{article.title}</a>
-      <p>{@html article.summary}</p> <!-- Use {@html} to render summary as HTML -->
-    </li>
+    <div class="row">
+      <div>
+        <a href={article.link} target="_blank">{article.title}</a>
+        <p>{@html article.summary}</p>
+      </div>
+    </div>
   {/each}
-</ul>
+</div>
