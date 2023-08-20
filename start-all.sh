00ad48cb6ed44f896de604727090d46b81fada94
mxpnt <maxpeuntinger@gmail.com>
2023-08-20 15:15:19 +0200

    kill all processes on ports before running

--- start-all.sh
+++ start-all.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Define the ports you want to free up
+ports=(8000 8080) # Add the ports used by your services
+
+# Kill any processes using those ports
+for port in "${ports[@]}"; do
+  pid=$(lsof -t -i:$port)
+  if [ -n "$pid" ]; then
+    echo "Killing process on port $port (PID: $pid)"
+    kill -9 $pid
+  fi
+done
+
+
 # Start Mosquitto
 sudo /etc/init.d/mosquitto start
 
